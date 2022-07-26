211b4f91906117adce1410a3d14beb641e7fa617
Aliyu Abubakar <sadiqful@gmail.com>
2022-07-26 12:35:15 +0000

    sad words

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+sad_words = ['sad', 'frustrated', 'anxious', 'tired', 'mad', 'depressed', 'down', 'angry', 'depressing', 'exhausting', 'low']
+
+starter_encouragements = [
+  'Hi there, you are absoluty amazing. Hang in there'
+  'Hey, you are brillant. You will get through this'
+  'You are amazing my dear friend. Do not be sad'
+  'You are loved'
+  'Cheer up'
+  'Hang in there'
+]
 
 def get_qoute():
   response = requests.get('https://zenquotes.io/api/random/')
@@ -26,6 +38,9 @@ async def on_message(message):
     if msg.startswith('$inspire'):
       qoute = get_qoute()
     await message.channel.send(qoute)
+
+    if any(word in msg for word in sad_words): 
+      await messsage.channel.send(random.choice(starter_encouragements))
 
 
 client.run(os.getenv('TOKEN'))
